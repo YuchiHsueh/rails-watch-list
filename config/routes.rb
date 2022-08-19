250c8374d7fb6666bdb361eeeff74cb8d5436d50
Yuchi Hsueh <bhsueh94@gmail.com>
2022-08-19 17:45:30 -0300

    setted index as root

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "lists#index"
   resources :bookmarks, only: [:destroy]
-  resources :lists, only: %i[index show new create] do
+  resources :lists, only: %i[show new create] do
     resources :bookmarks, only: %i[new create]
     resources :reviews, only: %i[new create]
   end
